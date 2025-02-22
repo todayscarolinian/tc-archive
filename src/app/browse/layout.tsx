c1662a4b8e621ffa0a98c4e8ab669bf7c27f2c5c
Jasper <jasper.apos@gmail.com>
2025-02-22 21:23:44 +0800

    feat: added comments for suggestions

--- src/app/browse/layout.tsx
+++ src/app/browse/layout.tsx
@@ -1,18 +1,22 @@
 import { Input } from "@/components/ui/input";
 
 type BrowseLayoutProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const BrowseLayout = ({ children }: BrowseLayoutProps) => {
   return (
     <section className="flex flex-col gap-8 pt-24 pb-34 px-4 md:px-8">
+      {/* 
+        For the Search feature, we could query and keep hitting the database or 
+        cache the data first and do simple filtering in the client side 
+      */}
       <div className="flex justify-center">
         <Input placeholder="Search issues..." className="w-full max-w-lg" />
       </div>
       {children}
     </section>
-  )
-}
+  );
+};
 
 export default BrowseLayout;

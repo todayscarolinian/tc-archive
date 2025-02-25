fc92e6d451759de8d0104d2fe4307183ccb2f959
Seaside <n.peteros2003@gmail.com>
2025-02-25 20:28:23 +0800

    feat: added whitelisting email and toast notifications upon sign in

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -6,7 +6,9 @@ import {
     User,
 } from "firebase/auth";
 
-import { auth } from "@/lib/firebase/clientApp";
+import { auth, db } from "@/lib/firebase/clientApp";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { toast } from "sonner";
 
 /**
  * Subscribes to the authentication state changes of the user.
@@ -20,21 +22,22 @@ export function onAuthStateChanged(cb: NextOrObserver<User>) {
     return _onAuthStateChanged(auth, cb);
 }
 
-/**
- * Initiates a Google sign-in process using a popup.
- *
- * This function uses the Firebase `signInWithPopup` method with a `GoogleAuthProvider`
- * to authenticate the user through Google. If the sign-in process is successful, the
- * user will be authenticated and their authentication state will be updated accordingly.
- *
- * @throws Will log an error to the console if the sign-in process fails.
- */
-
 export async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
 
     try {
-        await signInWithPopup(auth, provider);
+        const { user } = await signInWithPopup(auth, provider);
+
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("email", "==", user.email))
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+            toast.error("Access denied: Unauthorized email", { duration: 5000 });
+            await signOut();
+        } else {
+            toast.success("Successfully signed in", { duration: 5000 });
+        }
     } catch (error) {
         console.error("Error signing in with Google", error);
     }
